f95c92086a33e5f2c7fa3176ec636a558b6d1af7
camille1874 <15190971357@163.com>
2019-05-26 13:30:35 +0800

    update

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 # -*- encoding:utf-8 -*-
 import re
 from bert_serving.client import BertClient
+import numpy as np
 
 def get_encoding(lists):
-    bc = BertClient()
+    bc = BertClient(ip="219.223.189.238")
     return bc.encode(lists)
+
 
 def clean_str(string):
     string = re.sub(r"[(\-lrb\-)(\-rrb\-)]", "", string)
@@ -23,13 +25,14 @@ def clean_str(string):
     string = re.sub(r"\s{2,}", " ", string)
     return string.strip().lower() 
 
+
 def build_path(prefix, data_type, model_type, num_layers, postpix=""):
     return prefix + data_type + "-" + model_type + "-" + str(num_layers) + postpix
+
 
 def edit_dis(s1, s2):
     len1 = len(s1) + 1
     len2 = len(s2) + 1
-    # dis1 = [[0] * len2] * len1 乘法会导致引用
     dis = [[0] * len2 for i in range(len1)]
     for i in range(len1):
         dis[i][0] = i
@@ -43,4 +46,27 @@ def edit_dis(s1, s2):
                 dis[i][j] = min(dis[i - 1][j - 1], min(dis[i - 1][j], dis[i][j - 1])) + 1
     return dis[-1][-1] / max(len1, len2)
 
-#print(edit_dis("which position does sb play in baseball games", "position/play/baseball")) 0.54
+
+def cos_dis(s1, s2):
+    return s1.dot(s2)/(np.linalg.norm(s1)*np.linalg.norm(s2))
+
+
+def add_trans_bert(s1s, s2s, threthold=0.8):
+    bc = BertClient()
+    l1 = bc.encode(s1s)
+    l2 = bc.encode(s2s)
+    diss = []
+    for s1 in l1:
+        for s2 in l2:
+            diss.append(cos_dis(s1, s2))
+    aver_dis = sum(diss) / len(diss)
+    return aver_dis >= threthold
+
+
+def add_trans_edit(s1s, s2s, threshold=0.8):
+    diss = []
+    for s1 in s1s:
+        for s2 in s2s:
+            diss.append(edit_dis(s1, s2))
+    aver_dis = sum(diss) / len(diss)
+    return aver_dis >= threshold
